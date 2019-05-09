fbafdcf030d6b0cab930009c13960992c3a217b9
Oleksandr Tarasenko <oltar@vestas.com>
2019-05-09 16:31:18 +0200

    - Elliptic integral

--- src/test/python/elliptic_analog_filters_outputs.py
+++ src/test/python/elliptic_analog_filters_outputs.py
@@ -47,7 +47,7 @@ if __name__ == "__main__":
     #generateTestLowPassFilter(2, 10, 5, 40)
     #generateTestHighPassFilter(3, 10, 5, 40)
     #generateTestBandPassFilter(2, 10, 20, 5, 40)
-    print(special.ellipj(0.2, 0.9143))
+    print(special.ellipj(2.74707300, 0.93)) #2.747073004024667 1.5707963267948966
     #generateTestBandStopFilter(3, 10, 20, 5, 40)
 
 
--- src/test/scala/com/sparja/syto/filter/core/EllipticPrototypeTest.scala
+++ src/test/scala/com/sparja/syto/filter/core/EllipticPrototypeTest.scala
@@ -1,10 +1,12 @@
 package com.sparja.syto.filter.core
 
 import breeze.math.Complex
-import com.sparja.syto.common.Math.{PI, cos, sin, sqrt, asin}
-import com.sparja.syto.nuca.EllipticFunction.{ellipInc, am}
+import com.sparja.syto.common.Math.{PI, asin, cos, sin, sqrt}
+import com.sparja.syto.nuca.EllipticFunction.{am, ellipInc}
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
+
+import scala.collection.mutable.ListBuffer
 
 class EllipticPrototypeTest {
 
@@ -31,9 +33,11 @@ class EllipticPrototypeTest {
 
   def cd(u: Double, k: Double) = cn(u, k) / dn(u, k)
 
-  def K(k: Double) = ellipInc(asin(sqrt(k)), PI / 2)
+  def K(k: Double) = F(PI/2, k)
 
-  def findZero(u: Double, k: Double) = Complex.i / (k * cd(u * K(k), k))
+  def F(z: Double, k: Double) = ellipInc(asin(sqrt(k)), z)
+
+  def findZero(u: Double, k: Double) = Complex.i / (k * cd(u * K(k*k), k))
 
   @Test //(0.19748603438284257, 0.9803057003933715, 0.9820089760833982, 0.19879278412406867)
   def printEF() = {
@@ -43,10 +47,17 @@ class EllipticPrototypeTest {
    // println(sn(u, k))
    // println(cn(u, k))
    // println(dn(u, k))
-   // println(am(u, k))
-    println(cd(u * K(k), k))
+    println(am(u, sqrt(k)))
+   // println(cd(u * K(k), k))
   }
 
+  @Test
+  def compareOutputs() = {
+    val k = 0.93
+    println(K(k))
+    println(PI/2)
+    println(am(2.7470730, sqrt(k)))
+  }
 
   @Test
   def shouldFindZeros() = {
@@ -60,5 +71,84 @@ class EllipticPrototypeTest {
 
   }
 
+/*
+
+a[0] = 1.0;
+b = sqrt(1.0 - m);
+c[0] = sqrt(m);
+twon = 1.0;
+i = 0;
+
+while( fabs(c[i]/a[i]) > MACHEP )
+	{
+	if( i > 7 )
+		{
+		mtherr( "ellpj", OVERFLOW );
+		goto done;
+		}
+	ai = a[i];
+	++i;
+	c[i] = ( ai - b )/2.0;
+	t = sqrt( ai * b );
+	a[i] = ( ai + b )/2.0;
+	b = t;
+	twon *= 2.0;
+	}
+
+done:
+
+/* backward recurrence */
+phi = twon * a[i] * u;
+do
+	{
+	t = c[i] * sin(phi) / a[i];
+	b = phi;
+	phi = (asin(t) + phi)/2.0;
+	}
+while( --i );
+
+
+ */
+
+  @Test
+  def testCephesAm() = {
+    val k = 0.93
+    println(cephesAm(2.7470730, k))
+  }
+
+  def cephesAm(u: Double, k: Double) = {
+    var a = new Array[Double](9)
+    var c = new Array[Double](9)
+    a(0) = 1.0
+    var b = sqrt(1 - k)
+    c(0) = sqrt(k)
+    var twon = 1.0
+    var i = 0
+    var ai = 0.0
+    var t: Double = 0
+
+    while (c(i)/a(i) > 0.0000000001) {
+      ai = a(i)
+      i += 1
+      c(i) = ( ai - b )/2.0
+      t = sqrt( ai * b )
+      a(i) = ( ai + b )/2.0
+      b = t
+      twon *= 2.0
+    }
+
+    var phi = twon * a(i) * u
+
+
+    while(i != 0){
+      t = c(i) * sin(phi) / a(i);
+      b = phi;
+      phi = (asin(t) + phi)/2.0;
+      i -= 1
+    }
+
+    phi
+
+  }
 
 }
