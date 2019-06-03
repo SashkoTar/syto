4f417a99e2d041ee280885c03b3c8b62080f8d14
oltar <alex.tarasenko@yahoo.com>
2019-06-03 07:13:08 +0200

    -filter

--- src/test/python/filter_implementation.py
+++ src/test/python/filter_implementation.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 if __name__ == "__main__":
-    t = np.linspace(1, 20, 20)
+    t = np.linspace(1, 30, 30)
     #x = (np.sin(2*np.pi*0.75*t*(1-t) + 2.1) + 0.1*np.sin(2*np.pi*1.25*t + 1) + 0.18*np.cos(2*np.pi*3.85*t))
     #xn = x + np.random.randn(len(t)) * 0.08
     b, a = signal.butter(3, 0.3)
--- src/test/scala/com/sparja/syto/filter/FilterImplementationTest.scala
+++ src/test/scala/com/sparja/syto/filter/FilterImplementationTest.scala
@@ -21,12 +21,19 @@ class FilterImplementationTest {
   def filter(a: List[Double], b: List[Double], inputData: List[Int]) = {
     val output = new Array[Double](inputData.size)
 
-    def y(i: Int) = if (i >=0 && i < output.size - 1) output(i) else 0.0
+    def y(i: Int) = if (i >=0 && i <= output.size - 1) output(i) else 0.0
 
-    def x(i: Int) = if (i >=0 && i < inputData.size - 1) inputData(i) else 0
+    def x(i: Int) = if (i >=0 && i <= inputData.size - 1) inputData(i) else 0
 
    inputData.indices.foreach(n => {
-     output(n) = b(0)*x(n) + b(1)*x(n-1) + b(2)*x(n-2) + b(3)*x(n-3) - a(1)*y(n-1) - a(2)*y(n-2) - a(3)*y(n-3)
+     val xn = x(n)
+     val xn_1 = x(n-1)
+     val xn_2 = x(n-2)
+     val xn_3 = x(n-3)
+     val yn_1 = y(n-1)
+     val yn_2 = y(n-2)
+     val yn_3 = y(n-3)
+     output(n) = b(0)*xn + b(1)*xn_1 + b(2)*xn_2 + b(3)*xn_3 - a(1)*yn_1 - a(2)*yn_2 - a(3)*yn_3
      println(s"output($n)=${output(n)}")
    })
 
@@ -38,9 +45,19 @@ class FilterImplementationTest {
 
    println(filter(List(1, -1.16191748,  0.69594276, -0.1377613),
      List(0.049533, 0.14859899, 0.14859899, 0.049533),
-     (1 to 20).toList
+     (1 to 30).toList
    ))
 
+  }
+
+
+  @Test
+  def shouldFilterDataFor1() = {
+
+    println(filter(List(1, -1.16191748,  0.69594276, -0.1377613),
+      List(0.049533, 0.14859899, 0.14859899, 0.049533),
+      (1 to 30).toList
+    ))
 
   }
 
