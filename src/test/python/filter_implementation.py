6479bc7d6fe7b6fca3e28bfe25185321ff372ea7
Oleksandr Tarasenko <oltar@vestas.com>
2019-06-14 08:57:14 +0200

    - Filter implementation

--- src/test/python/filter_implementation.py
+++ src/test/python/filter_implementation.py
@@ -3,18 +3,30 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def lfilter_(b, a, y):
+    return signal.lfilter(b, a, y)
+
+
+def filtfilt_(b, a, y):
+    return signal.filtfilt(b, a, y)
 
 
 if __name__ == "__main__":
-    t = np.linspace(1, 20, 20)
+    end = 20
+    t = np.linspace(1, end, end)
     #x = (np.sin(2*np.pi*0.75*t*(1-t) + 2.1) + 0.1*np.sin(2*np.pi*1.25*t + 1) + 0.18*np.cos(2*np.pi*3.85*t))
     #xn = x + np.random.randn(len(t)) * 0.08
     b, a = signal.butter(3, 0.3)
-  #  zi = signal.lfilter_zi(b, a)
-  #  print(zi)
+    #zi = signal.lfilter_zi(b, a)
+    #print(zi)
     print(b)
     print(a)
-    res = signal.lfilter(b, a, t)
+    res = lfilter_(b, a, t)
+    print("LFILT:")
+    print(res)
+
+    res = filtfilt_(b, a, t)
+    print("FILTFILT")
     print(res)
 
 
